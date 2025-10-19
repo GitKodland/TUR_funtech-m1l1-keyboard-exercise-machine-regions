59df0c44065ffac8b3765a5b50f4868506047ea5
GitKodland <125306926+GitKodland@users.noreply.github.com>
2025-10-19 13:55:52 +0300

    Update texts.js

--- js/texts.js
+++ js/texts.js
@@ -1,14 +1,14 @@
 const TEXTS = {
   base: {
-    hintTitle: "Hint",
-    task1: "Find the first letter of your name and press it! Then press <b>Check</b> or <b>Enter</b>.",
-    task2: "Type the word <b>{CAT}</b>. Then press <b>Check</b> or <b>Enter</b>.",
-    task3: "Press <b>Space</b> — make the cat walk forward. Collect 2 cats! 🐾",
-    end: "You completed all tasks, great job! 🎉",
-    finishHint: "All tasks completed! 🎉 Great job! Press <b>Restart</b> to play again.",
-    check: "Check",
-    restart: "Restart",
-    finishTitle: "You completed all tasks, great job!",
+    hintTitle: "İpucu",
+    task1: "Adınızın ilk harfini bulun ve o harfe basın! Sonrasında <b>Kontrol Et</b> butonuna tıklayın ya da <b>Enter</b> tuşuna basın.",
+    task2: "Kelimeyi yazın: <b>{CAT}</b>. Sonrasında <b>Kontrol Et</b> butonuna tıklayın ya da <b>Enter</b> tuşuna basın.",
+    task3: "<b>Boşluk</b> tuşuna basın — Kedinin ileri gitmesini sağlayın. 2 kedi toplayın! 🐾",
+    end: "Tüm görevleri tamamladınız, tebrikler! 🎉",
+    finishHint: "Tüm görevler tamamlandı!! 🎉 Harikasınız! <b>Yeniden Başlat</b> butonuna tıklayarak tekrar oynayabilirsiniz.",
+    check: "Kontrol Et",
+    restart: "Yeniden Başlat",
+    finishTitle: "Tüm görevleri tamamladınız, tebrikler!",
     catName: "Cat", // DO NOT TRANSLATE - FOR ALL REGIONS WE USE THE WORD 'CAT' - in English!
     targetWord: "CAT" // DO NOT TRANSLATE - FOR ALL REGIONS WE USE THE WORD 'CAT' - in English!
   },
